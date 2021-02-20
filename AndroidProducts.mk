643e5dc114e297bbc8599e24ee58aafad2fceb37
Nima Mohammadi <nima__mohammadi@outlook.com>
2021-02-21 00:12:05 +0100

    adding device definition and lunch target

---
+++ AndroidProducts.mk
@@ -0,0 +1,6 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/beagle_black.mk
+
+COMMON_LUNCH_CHOICES := \
+    beagle_black-userdebug
+
---
+++ beagle_black.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_NAME := beagle_black
+PRODUCT_DEVICE := beagle_black
+PRODUCT_BRAND := bbbrevc
+PRODUCT_MODEL :=  Beaglebone Black RevC
+PRODUCT_MANUFACTURER := TI
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+PRODUCT_AAPT_PREF_CONFIG := hdpi
